0b9c81652a5fe891e19069eb2ed023186ffe8d77
MoritzRoth <moritz.roth98@gmail.com>
2018-09-10 17:50:20 +0200

    added functions vert_to_asset and asset_on_lines which set assets on vertices and lines respectively

--- retourmiddleware/assetpos/views.py
+++ retourmiddleware/assetpos/views.py
@@ -3,7 +3,8 @@ import os.path
 from .shp_reader import *
 import json
 
-from osgeo import gdal
+#from osgeo import gdal
+from osgeo import ogr
 
 # Create your views here.
 def index(request):
@@ -20,10 +21,22 @@ def index(request):
             data = json.load(f)
         return JsonResponse(data)
     elif os.path.isfile(filePath):
-        print("opening ",filePath)
-        BASE = os.path.dirname(os.path.abspath(__file__))
-        dataset = gdal.Open(os.path.join(BASE,filePath))
+        print("opening %s" % filePath)
 
-        return JsonResponse(calcAssetPos(dataset))
+        driver = ogr.GetDriverByName('ESRI Shapefile')
+        if driver is None:
+            return JsonResponse({"Error": "driver not available ESRI Shapefile"})
+
+        dataset = driver.Open(filePath, 0)
+
+        if dataset is None:
+            return JsonResponse({"Error": "could not open "+filePath})
+        else:
+            print("opened %s" % filePath)
+            data = calcAssetPos(dataset,request.path)
+            with open(genfilePath, 'w') as outfile:
+                json.dump(data,outfile)
+
+            return JsonResponse(data)
     else:
-        return JsonResponse({"Error": "file " +fileName+ ".shp does not exist"})
+        return JsonResponse({"Error": "file %s.shp does not exist" % fileName})
