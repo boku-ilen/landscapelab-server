4e914e5d517984df8a4346fe11e05c1900653522
chrisgraf <christoph.graf@boku.ac.at>
2018-12-05 17:11:20 +0100

    * removed dhm from available global urls

--- retourmiddleware/urls.py
+++ retourmiddleware/urls.py
@@ -4,7 +4,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('dhm/', include('dhm.urls')),
     path('assetpos/', include('assetpos.urls')),
     path('buildings/', include("buildings.urls")),
     path('location/', include("location.urls")),
