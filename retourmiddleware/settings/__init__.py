201226b6af32997e2adcb05e278ec4b2b532032c
graf <christoph.graf@boku.ac.at>
2018-12-13 13:07:04 +0100

    * check if the local_settings.py file exists and potentially warns if running without a working configuration * restructured default_settings.py (removed some standard comments)

--- retourmiddleware/settings/__init__.py
+++ retourmiddleware/settings/__init__.py
@@ -1,6 +1,17 @@
 from split_settings.tools import optional, include
+import logging
+import os
 
+logger = logging.getLogger(__name__)
+LOCAL_SETTINGS = "local_settings.py"
+
+# setup the django settings
 include(
     'default_settings.py',
-    optional('local_settings.py')
+    optional(LOCAL_SETTINGS)
 )
+
+# give a warning if the optional local settings are not found
+if not os.path.isfile(LOCAL_SETTINGS):
+    logger.warning("could not find local settings file in {}"
+                   "- are you sure your setup is correct?".format(LOCAL_SETTINGS))
--- retourmiddleware/settings/default_settings.py
+++ retourmiddleware/settings/default_settings.py
@@ -16,16 +16,12 @@ import logging.config
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
+# TODO: development setup (https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/)
 SECRET_KEY = "development"
-
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
 ALLOWED_HOSTS = []
 
 # Application definition
-
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -72,8 +68,7 @@ TEMPLATES = [
 WSGI_APPLICATION = 'retourmiddleware.wsgi.application'
 
 # Database
-# https://docs.djangoproject.com/en/2.0/ref/settings/#databases
-
+# FIXME: does an sqlite default connection make sense now as we require postgis?
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -82,8 +77,6 @@ DATABASES = {
 }
 
 # Password validation
-# https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
-
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
@@ -100,8 +93,6 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 # Internationalization
-# https://docs.djangoproject.com/en/2.0/topics/i18n/
-
 LANGUAGE_CODE = 'en-us'
 TIME_ZONE = 'UTC'
 USE_I18N = True
@@ -109,10 +100,7 @@ USE_L10N = True
 USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/2.0/howto/static-files/
-
 STATIC_URL = '/static/'
-
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "resources")
 ]
