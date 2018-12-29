a6630f700c0ec93330dd92fac0cbbd569ed7f429
graf <christoph.graf@boku.ac.at>
2018-12-30 00:32:26 +0100

    * added a request url for the draft of the assetpos implementation

--- assetpos/urls.py
+++ assetpos/urls.py
@@ -6,4 +6,9 @@ from . import views
 urlpatterns = [
     # path('',views.index, name='index'),
     url(r'^', views.index, name="index"),
+
+    # request the locations of an assettype and a given tile
+    url(r'^get/(?P<assettype_id>\s)/(?P<tile_x>(\d+))/(?P<tile_y>(\d+))/(?P<zoom>(\d+)).json$',
+        views.get_assetposition, 'get_assetposition'),
+
 ]
