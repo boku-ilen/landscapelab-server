7f1baa2c022c2cfce1a42e683c354d30c948af5e
Karl Bittner <karl.bittner@outlook.com>
2019-02-13 11:41:39 +0100

    Proper height offset in spritesheet; added comments and improved documentation in vegetation_spritesheet.py

--- vegetation/vegetation_spritesheet.py
+++ vegetation/vegetation_spritesheet.py
@@ -15,26 +15,35 @@ logger = logging.getLogger(__name__)
 
 
 def generate_spritesheet(id, layer):
-    """Generates the spritesheet with all plant images for a given phytocoenosis ID and layer."""
+    """Generates the spritesheet containing all plant images for a given phytocoenosis ID and layer.
+
+    Example: With id=2 and layer=K, the resulting path would be /phytocoenosis-spritesheet/2/K.png
+    """
 
     # TODO: Layer is not actually used yet!
     representations = get_object_or_404(Phytocoenosis, id=id).speciesRepresentations.all()
     sprite_paths = [rep.billboard for rep in representations]
 
+    # Open all sprites at the sprite_paths using Pillow
     sprites = list(map(Image.open, sprite_paths))
     widths, heights = zip(*(s.size for s in sprites))
 
+    # The maximum width and height are used for the spritesheet in order to fit in all images
     max_width = max(widths)
     max_height = max(heights)
 
     spritesheet = Image.new('RGBA', (max_width * len(sprite_paths), max_height))
 
+    # Iterate over all sprites and paste them in the spritesheet, with an increasing x_offset, at the correct height
+    # to have every sprite reach the bottom of the image
     x_offset = 0
 
     for sprite in sprites:
-        spritesheet.paste(sprite, (x_offset, 0))
+        width, height = sprite.size
+        spritesheet.paste(sprite, (x_offset, max_height - height))
         x_offset += max_width
 
+    # Create all required directories if they don't yet exist
     pathset = SPRITE_PATHSET.format(id)
 
     if not (os.path.exists(SPRITE_BASEPATH)):
@@ -43,6 +52,7 @@ def generate_spritesheet(id, layer):
     if not (os.path.exists(pathset)):
         os.mkdir(pathset)
 
+    # Save the spritesheet
     filename = SPRITE_FILE.format(id, layer)
     spritesheet.save(filename)
 
