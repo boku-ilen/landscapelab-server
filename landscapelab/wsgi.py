c070a7775451031105313a2e289b0efd36bf3dd5
graf <christoph.graf@boku.ac.at>
2019-03-19 21:40:51 +0100

    * added root directory to wsgi configuration so that all modules can be * imported

--- landscapelab/wsgi.py
+++ landscapelab/wsgi.py
@@ -1,16 +1,15 @@
 """
-WSGI config for retourmiddleware project.
+WSGI configuraton for the landscapelab-server
 
 It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
 
+sys.path.append(os.path.dirname(os.path.abspath(__file__)) + '/..')
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "landscapelab.settings")
 
 application = get_wsgi_application()
