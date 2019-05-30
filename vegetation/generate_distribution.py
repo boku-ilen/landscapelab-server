97f4d7e563a18964f879af6d7cb5ab4e5a807585
Karl Bittner <karl.bittner@outlook.com>
2019-05-31 01:46:58 +0200

    Improved vegetation distribution generation with no empty pixels
    The previos implementation created a huge number of empty pixels due to how the layer distributions were created and how random numbers were picked. This commit fixes this, causing a big performance improvement (and much denser vegetation) in the client.

--- vegetation/generate_distribution.py
+++ vegetation/generate_distribution.py
@@ -42,40 +42,37 @@ def get_density_img_array(species):
     """Returns an array with random positions for each species, taking the density into
     account (but not the clumping behavior).
 
-    Plants are selected like this:
-    If there is only one SpeciesRepresentation with full density (1), the chance for it
-    to spawn is 50:50. If there are two SpeciesRepresentations with full density, the
-    chance for one of them is 1/3. If there are two SpeciesRepresentations, one with
-    full density, another with a density of 1/2, the chance for the one with full density
-    is 1/3, and the chance for the one with half density is 1/6. etc.
+    Plants at each pixel are selected by picking random numbers for each species, multiplied
+    by the density. The species with the highest number gets inserted.
     """
 
-    number_of_species = len(species)
+    ids = [sp.id for sp in species]
 
     img_data = []
 
     # Generate a random value for each pixel
     for x in range(0, IMG_SIZE ** 2):
         new_id = 0
+        highest_roll = 0
 
         for s in species:
-            dice = random.randint(0, int(1 / (s.distribution_density / number_of_species)))
-            if dice == 1:
+            dice = random.random() * s.distribution_density
+            if dice >= highest_roll:
                 new_id = s.id
-                break
 
-        img_data.append(new_id)
+        img_data.append(ids.index(new_id) if new_id > 0 else 0)
 
     return img_data
 
 
-def generate_distribution_for_phytocoenosis(phyto_c_id):
-    """Generates and saves a distribution image for a whole phytocoenosis.
+def generate_distribution_for_phytocoenosis_and_layer(phyto_c_id, layer):
+    """Generates and saves a distribution image for a phytocoenosis at the given layer.
 
     The pixel's red values correspond to the speciesRepresentations, starting at 1. A value
     of 0 means that no plant should be placed at that location.
 
-    Example: With pid=2, the image is saved to /phytocoenosis-distribution/2/complete.png
+    Example: With phyto_c_id=2 and layer=5, the image is saved to
+    /phytocoenosis-distribution/2/5.png
     """
 
     # Create all required directories if they don't yet exist
@@ -88,63 +85,15 @@ def generate_distribution_for_phytocoenosis(phyto_c_id):
         os.mkdir(pathset)
 
     # Get the speciesRepresentations in the phytocoenosis
-    species = get_object_or_404(Phytocoenosis, id=phyto_c_id).speciesRepresentations.all()
+    species = get_object_or_404(Phytocoenosis, id=phyto_c_id) \
+        .speciesRepresentations.filter(vegetation_layer=layer).all()
 
     # Create an image with randomly spread values for the species IDs
     img = Image.new("L", size=(IMG_SIZE, IMG_SIZE))
     img.putdata(get_density_img_array(species))
 
-    filename = DISTRIBUTION_PATH.format(phyto_c_id, "complete")
+    filename = DISTRIBUTION_PATH.format(phyto_c_id, layer)
     img.save(filename)
-
-
-def generate_distribution_for_phytocoenosis_and_layer(pytho_c_id, layer):
-    """Generates and saves a distribution image for a phytocoenosis at a specific layer.
-    If there is no complete distribution image yet, it is generated first.
-    """
-
-    # Create all required directories if they don't yet exist
-    pathset = DISTRIBUTION_PATHSET.format(pytho_c_id)
-
-    if not (os.path.exists(DISTRIBUTION_BASE)):
-        os.mkdir(DISTRIBUTION_BASE)
-
-    if not (os.path.exists(pathset)):
-        os.mkdir(pathset)
-
-    # Fetch the complete generation, generate it first if necessary
-    complete_filename = DISTRIBUTION_PATH.format(pytho_c_id, "complete")
-
-    if not os.path.isfile(complete_filename):
-        generate_distribution_for_phytocoenosis(pytho_c_id)
-
-    complete_image = Image.open(complete_filename)
-
-    # Get the IDs in this layer
-    representations = get_object_or_404(Phytocoenosis, id=pytho_c_id)\
-        .speciesRepresentations.filter(vegetation_layer=layer).all()
-    ids = [rep.id for rep in representations]
-
-    # Create a new image and paste the complete one
-    layer_filename = DISTRIBUTION_PATH.format(pytho_c_id, layer)
-    layer_image = Image.new("L", size=(IMG_SIZE, IMG_SIZE))
-    layer_image.paste(complete_image)
-
-    pixels = layer_image.load()
-
-    # Replace pixels so that, instead of the phytocoenosis ID, they become the layer ID
-    for x in range(IMG_SIZE):
-        for y in range(IMG_SIZE):
-            if pixels[x, y] in ids:
-                # Replace
-                new = 1 + ids.index(pixels[x, y])
-                pixels[x, y] = new
-            else:
-                # Nothing here
-                pixels[x, y] = 0
-
-    # Save the image
-    layer_image.save(layer_filename)
 
 
 def get_distribution_for_id_and_layer(phyto_c_id, layer):
