ef775cd8779db51623c0b1f6aacf9d838549ca9d
karl <karl.bittner@hexaquo.at>
2019-08-15 11:10:20 +0200

    Skip correctly named tiles in tms_to_osm_tilenames
    With the do_austria_check flag, only tiles which are renamed incorrectly
    for Austria are renamed, others are skipped.
    
    The check works for Austria because there we can expect the x coordinate
    to be bigger than the y coordinate.

--- raster/management/commands/tms_to_osm_tilenames.py
+++ raster/management/commands/tms_to_osm_tilenames.py
@@ -7,10 +7,13 @@ from django.core.management import BaseCommand
 
 
 class Command(BaseCommand):
-    help = 'Converts pyramid tiles from TMS format to OSM by transforming the y coordinate of the tile paths.'
+    help = 'Converts pyramid tiles from TMS format to OSM by transforming the y coordinate of the tile paths. ' \
+           'If the do_austria_check flag is set, only tiles which seem to have the incorrect format for Austrian' \
+           ' tiles are renamed.'
 
     def add_arguments(self, parser):
         parser.add_argument('--pyramidpath', type=str)
+        parser.add_argumemt('--do_austria_check', action='store_true')
 
     def handle(self, *args, **options):
         pyramidpath = options['pyramidpath']
@@ -26,6 +29,8 @@ class Command(BaseCommand):
             for x_folder in os.listdir(full_zoom_path):
                 full_x_path = os.path.join(full_zoom_path, x_folder)
 
+                x_coordinate = int(x_folder)
+
                 for y_file in os.listdir(full_x_path):
                     full_y_file = os.path.join(full_x_path, y_file)
 
@@ -34,11 +39,19 @@ class Command(BaseCommand):
                         coordinate_and_ending = y_file.split('.', 1)
 
                         # Transform the y coordinate to OSM format
-                        coordinate = int(coordinate_and_ending[0])
-                        new_y_coordinate = (1 << zoom) - coordinate - 1
+                        y_coordinate = int(coordinate_and_ending[0])
+
+                        if options['do_austria_check']:
+                            # In Austria, the y coordinate of correctly named tiles is always smaller than the x
+                            # coordinate. If that's the case, don't do anything
+                            if x_coordinate > y_coordinate:
+                                continue
+
+                        new_y_coordinate = (1 << zoom) - y_coordinate - 1
                         new_y_file = str(new_y_coordinate) + '.' + str(coordinate_and_ending[1])
 
                         # Build the new filename and rename the file
                         full_new_file = os.path.join(full_x_path, new_y_file)
                         os.rename(full_y_file, full_new_file)
+
                         print("Renamed {} to {}".format(full_y_file, full_new_file))
