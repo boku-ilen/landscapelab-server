9eaf6064d45a415cae1d87eca0efc455cd97935f
karl <karl.bittner@hexaquo.at>
2019-08-26 15:26:08 +0200

    Make placeholder energy contribution dynamic
    The energy contribution is still a placeholder and the IDs are
    hardcoded, but the number_of_assets is correct (and the energy
    contribution depends on it)

--- assetpos/views.py
+++ assetpos/views.py
@@ -156,16 +156,21 @@ def get_energy_contribution(request, asset_type_id=None):
     """Returns a json response with the energy contribution and number of contributing
      assets, either for a given asset type or for all assets."""
 
-    # TODO: Placeholders
-
     ret = {
-        "total_energy_contribution": 300,
-        "number_of_assets": 15
+        "total_energy_contribution": 0,
+        "number_of_assets": 0
     }
 
     if asset_type_id:
-        ret["number_of_assets"] = 5
-        ret["total_energy_contribution"] = 100
+        asset_count = len(AssetPositions.objects.filter(asset_type=asset_type_id).all())
+    else:
+        # TODO: If we get all, Buildings are included, so the IDs are hardcoded for this placeholder
+        #  We may need an additional field (or model?) for energy contributing assets in the future
+        asset_count = len(AssetPositions.objects.filter(asset_type=2).all())\
+                      + len(AssetPositions.objects.filter(asset_type=3).all())
+
+    ret["number_of_assets"] = asset_count
+    ret["total_energy_contribution"] = asset_count * 10  # TODO: Placeholder energy contribution
 
     return JsonResponse(ret)
 
