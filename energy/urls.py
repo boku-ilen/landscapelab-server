969e235940eb822ca0dbaf3fbf78599daa2d71fd
karl <karl.bittner@hexaquo.at>
2019-09-05 16:58:37 +0200

    Add 'energy' urls to project, they work now

--- energy/urls.py
+++ energy/urls.py
@@ -2,14 +2,16 @@ from django.conf.urls import url
 
 from . import views
 
-# request the energy production of a certain location
-url(r'^location/(?P<asset_position_id>(\d+)).json$',
-    views.get_energy_by_location, name='get_energy_by_location'),
+urlpatterns = [
+    # request the energy production of a certain location
+    url(r'^location/(?P<asset_position_id>(\d+)).json$',
+        views.get_energy_by_location, name='get_energy_by_location'),
 
-# Request energy contribution of an asset type
-url(r'^contribution/(?P<scenario_id>(\d+))/(?P<asset_type_id>(\d+)).json$',
-    views.get_energy_contribution, name='get_energy_contribution_asset_type'),
+    # Request energy contribution of an asset type
+    url(r'^contribution/(?P<scenario_id>(\d+))/(?P<asset_type_id>(\d+)).json$',
+        views.get_energy_contribution, name='get_energy_contribution_asset_type'),
 
-# Request energy contribution of all assets
-url(r'^contribution/(?P<scenario_id>(\d+))/all.json$',
-    views.get_energy_contribution, name='get_energy_contribution_global'),
+    # Request energy contribution of all assets
+    url(r'^contribution/(?P<scenario_id>(\d+))/all.json$',
+        views.get_energy_contribution, name='get_energy_contribution_global')
+]
--- landscapelab/urls.py
+++ landscapelab/urls.py
@@ -7,10 +7,11 @@ from landscapelab import utils
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('assetpos/', include('assetpos.urls')),
-    path('buildings/', include("buildings.urls")),
-    path('location/', include("location.urls")),
-    path('raster/', include("raster.urls")),
-    path('vegetation/', include("vegetation.urls")),
-    path('linear/', include("linear.urls")),
+    path('buildings/', include('buildings.urls')),
+    path('location/', include('location.urls')),
+    path('raster/', include('raster.urls')),
+    path('vegetation/', include('vegetation.urls')),
+    path('linear/', include('linear.urls')),
+    path('energy/', include('energy.urls')),
     url(r'^reload_logging', utils.reload_logging),
 ]
