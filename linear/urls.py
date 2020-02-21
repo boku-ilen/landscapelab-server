bb49706b234af568237232b6a7a77ca2f6684c38
karl <karl.bittner@hexaquo.at>
2020-02-21 15:09:43 +0100

    Add function+url to get linear data for a tile

--- linear/urls.py
+++ linear/urls.py
@@ -5,6 +5,9 @@ from . import views
 urlpatterns = [
 
     url(r'^(?P<position_x>(\d+(?:\.\d+)))/(?P<position_y>(\d+(?:\.\d+)))/(?P<line_type_id>(\d+)).json$',
-        views.get_lines_near_position, name="get_lines_near_position")
+        views.get_lines_near_position, name="get_lines_near_position"),
+
+    url(r'^tile/(?P<line_type_id>(\d+))/(?P<tile_x>(\d+(?:\.\d+)))/(?P<tile_y>(\d+(?:\.\d+)))/(?P<zoom>(\d+)).json$',
+        views.get_lines_for_tile, name="get_lines_for_tile")
 
 ]
--- linear/views.py
+++ linear/views.py
@@ -1,8 +1,42 @@
+import webmercator
 from django.contrib.gis import geos
+from django.contrib.gis.geos import Polygon
 from django.http import JsonResponse
 
 from linear.models import LineType, LineSegment
 import logging
+
+
+def get_lines_for_tile(request, line_type_id, tile_x, tile_y, zoom):
+    ret = {}
+
+    line_type_id = int(line_type_id)
+
+    # Construct the polygon which represents this tile, filter assets with that polygon
+    point = webmercator.Point(meter_x=float(tile_x), meter_y=float(tile_y), zoom_level=int(zoom))
+    tile_center = webmercator.Point(tile_x=point.tile_x, tile_y=point.tile_y, zoom_level=int(zoom))
+
+    polygon = Polygon((
+        (tile_center.meter_x + tile_center.meters_per_tile / 2, tile_center.meter_y + tile_center.meters_per_tile / 2),
+        (tile_center.meter_x + tile_center.meters_per_tile / 2, tile_center.meter_y - tile_center.meters_per_tile / 2),
+        (tile_center.meter_x - tile_center.meters_per_tile / 2, tile_center.meter_y - tile_center.meters_per_tile / 2),
+        (tile_center.meter_x - tile_center.meters_per_tile / 2, tile_center.meter_y + tile_center.meters_per_tile / 2),
+        (tile_center.meter_x + tile_center.meters_per_tile / 2, tile_center.meter_y + tile_center.meters_per_tile / 2)),
+        srid=3857)
+
+    segments = LineSegment.objects.filter(type=line_type_id, line__intersects=polygon)
+
+    # Add the segments to the response
+    for segment in segments.all():
+        # Ensure WebMercator coordinates
+        line = segment.line
+        line.transform(3857)
+
+        ret[segment.id] = {
+            "line": line.coords,
+            "width": segment.width}
+
+    return JsonResponse(ret)
 
 
 def get_lines_near_position(request, position_x, position_y, line_type_id):
